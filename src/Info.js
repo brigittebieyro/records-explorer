ffde65b58552a8c027756c168c2f675d0a2c6ca1
Brigitte Bieyro <brigittebieyro@Brigittes-MacBook-Pro-2.local>
2026-01-02 01:02:04 -0800

    Add the info file!

---
+++ src/Info.js
@@ -0,0 +1,18 @@
+function Info() {
+  return (
+    <div className="App">
+      <div className="info-page">
+        <h2>About Records</h2>
+        <p>
+         Each level of weightlifting has records, and while the local level are the ones this site is focused on, you may also be interested in working towards a national or international records. National records and standards are maintained by USAW, while international records are maintained by multiple governing bodies: the IWF maintains international records for youth, junior, and senior athletes. For masters lifters, there are two governing bodies with their own set of records: the UMWF and the IMWA.
+        </p>
+        <p>
+            So what happens when an athlete breaks a record? And whose job is it to notice and ensure the record is recognized? 
+        </p>
+<p><strong>Local Records:</strong><br />At the local level, the WSO committee is keeping an eye out, but we're not all seeing, and do not get automatic notifications. Athletes and coaches who believe they've set a record are encouraged to reach out to the WSO committee and let us know. </p>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
